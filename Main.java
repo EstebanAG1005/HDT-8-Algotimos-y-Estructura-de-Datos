aa4d25d1876c082854edf6dae71785356d83a39d
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-04-19 10:16:41 -0600

    cambios en Programa

--- Main.java
+++ Main.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-    
-            //Data contiene a todos los pacientes 
-            ArrayList<Paciente> Data = new ArrayList<>();    
-            int op = 0;
-            Scanner scan = new Scanner(System.in);
-            boolean salir = true;
 
-        while(true){
-            try{
+            //Data contiene a todos los pacientes 
+            ArrayList<Paciente> Data = new ArrayList<>();
+            int op = 0;
+            boolean salir = true;
+            Scanner scan = new Scanner(System.in);
+            while (salir) {
+                // Prints the main menu by default, JCF implementation is used
                 System.out.println("-----------------Hoja de Trabajo #8 --------------------------------");
                 System.out.println("-----------------------------------------------------------------------------------------");
                 System.out.println(" Bienvenido al Programa, Por favor Seleccione que tipo de implementacion desea realizar: ");
@@ -22,71 +21,65 @@ public class Main {
                 System.out.println("2. Implementacion de JCF");
                 System.out.println("3. Salir");
                 System.out.println("-----------------------------------------------------------------------------------------");
-                op = scan.nextLine();
+                op = scan.nextInt();
 
+                // Reads the document
                 try {
                     BufferedReader reader = new BufferedReader(new FileReader("pacientes.txt"));
                     String line;
-        
+
                     // Saves each patient found
-                    while((line = reader.readLine()) != null) {
+                    while ((line = reader.readLine()) != null) {
                         String patientIntel[] = line.split(",");
                         Paciente patient = new Paciente(patientIntel[0], patientIntel[1], patientIntel[2]);
                         Data.add(patient);
                     }
-        
-                // If document is not found
+
+                    // If document is not found
                 } catch (Exception E) {
                     System.err.println("Hubo un error al leer el documento");
                     System.err.println("Por favor asegurese de que el nombre de este sea 'pacientes.txt'");
                 }
 
-                if(op == 1){
+                // VectorHeap implementation
+                if (op == 1) {
                     System.out.println("Utilizando VectorHeap con PriorityQueue");
-                VectorHeap<Paciente> patientHeap = new VectorHeap<>();  // Using VectorHeap with PriorityQueue
-    
-                // Adds all patients to the heap
-                for(int i = 0; i < Data.size(); i++) {
-                    patientHeap.add(Data.get(i));
-                }
-            }
-    
-                // Returns the patients according to priority
-                while (patientHeap.size() > 0){
-                    Paciente currentPatient = patientHeap.remove();
-                    attendPatient(currentPatient);
-                }
-                if(op == 2){
+                    VectorHeap<Paciente> patientHeap = new VectorHeap<>();  // Using VectorHeap with PriorityQueue
+
+                    // Adds all patients to the heap
+                    for (int i = 0; i < Data.size(); i++) {
+                        patientHeap.add(Data.get(i));
+                    }
+
+                    // Returns the patients according to priority
+                    while (patientHeap.size() > 0) {
+                        Paciente currentPatient = patientHeap.remove();
+                        attendPatient(currentPatient);
+                    }
+
+                    // JCF implementation
+                } else if (op == 2) {
                     System.out.println("Utilizando Java Collections Framework");
                     PriorityQueue<Paciente> patientQueue = new PriorityQueue<Paciente>();   // Using JCF
-        
+
                     // Adds all patients to the queue
-                    for(int i = 0; i < Data.size(); i++) {
+                    for (int i = 0; i < Data.size(); i++) {
                         patientQueue.add(Data.get(i));
                     }
-        
+
                     // Returns the patients according to priority
-                    while(patientQueue.size() > 0) {
+                    while (patientQueue.size() > 0) {
                         Paciente currentPatient = patientQueue.remove();
                         attendPatient(currentPatient);
                     }
-                }
-                else if (op == 3){
-                    System.out.println("Gracias por usar el programa, Que tenga un buen dia :) ");
-                    salir = false;
-                    System.Exit(0);
-                }
-                else{
-                    System.out.println("Ingrese un numero que se encuentre en el rango correspondiente");
-                }
-                }
-                catch (Exception e) {
-                    System.out.println("Solo se pueden ingresar numeros\n");
-                    scan.next();
+                } else if (op == 3) {
+                    System.out.println("Gracias por usar el programa, Que tenga un buen dia :)");
+                    System.exit(0);
                 }
             }
-
-            /**
+        }
+    
+        /**
          * Method that displays the current patient and waits for the next one
          */
         public static void attendPatient(Paciente patient) {
@@ -100,7 +93,7 @@ public class Main {
     
             // Prints needed data
             System.out.println("-----------------------------------------------------------------------------------------");
-            System.out.println("Atendiendo paciente");
+            System.out.println("Atendiendo al paciente");
             System.out.println("Nombre: " + nombre);
             System.out.println("Padecimientos:" + sintomas);
             System.out.println("Prioridad:" + prioridad);
@@ -109,8 +102,7 @@ public class Main {
             System.out.println();
             // Waits for confirmation that the doctor is finished
             System.out.println("Presione enter cuando desee atender al siguiente");
-            System.out.print(">>");
             next.nextLine();
         }
     }
-}
+
